6b8760c2432ada6d136b556ec78472181fd970c5
trhgquan <quanthcstpbt@gmail.com>
2020-07-06 22:43:36 +0700

    them menu thay doi thong tin cho nguoi dung.

--- LibManager/Menu.h
+++ LibManager/Menu.h
@@ -8,11 +8,24 @@
 #define CHANGE_PASSWORD_COMMAND_CODE 9455 // P - A - S -S.
 #define CHANGE_INFO_COMMAND_CODE 1024 // nonsense, se set gia tri khac trong tuong lai.
 
+// Command menu thay doi thong tin nguoi dung.
+// Menu thong tin co the nhan trung gia tri voi menu ben tren,
+// nhung xu ly 2 cai nay rieng nen khong xung dot.
+#define MENU_DOI_TEN_COMMAND_CODE 1
+#define MENU_DOI_NGAY_SINH_COMMAND_CODE 2
+#define MENU_DOI_CMND_COMMAND_CODE 3
+#define MENU_DOI_DIA_CHI_COMMAND_CODE 4
+#define MENU_DOI_GIOI_TINH_COMMAND_CODE 5
+#define MENU_THOAT_DOI_COMMAND_CODE 6
+
 // Chua dang nhap
 void nonLoggedInMenu();
 
 // Menu co ban.
 void generalMenu();
+
+// Menu thay doi thong tin nguoi dung.
+void editInfoMenu();
 
 // Menu cho admin.
 void adminMenu();
